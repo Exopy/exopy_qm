76c0cae747e23f661820a6b395eff1961a160982
AntoineEssig <79255346+AntoineEssig@users.noreply.github.com>
2021-03-01 16:40:16 +0100

    Antoine Essig (#19)
    * Save the OPX data in one recarray named "Results". The keys of Results are the name of the data saved. Add Results to the database. Using the save function with Results will create an entry in the HDF5 file for every keys in the recarray.
    
    * Keep the old individual entries in the database for all OPX saved data. This will be removed soon so please migrate to the Results recarry.

--- exopy_qm/tasks/tasks/ConfigureExecuteTask.py
+++ exopy_qm/tasks/tasks/ConfigureExecuteTask.py
@@ -4,9 +4,10 @@ import importlib.util
 import logging
 from pathlib import Path
 import shutil
+from numpy import zeros
 
 import qm.qua
-from atom.api import List, Typed, Str, Value, Bool
+from atom.api import List, Typed, Str, Value, Bool, set_default
 from exopy.tasks.api import InstrumentTask
 
 logger = logging.getLogger(__name__)
@@ -64,6 +65,9 @@ class ConfigureExecuteTask(InstrumentTask):
 
     #: Doesn't wait for the program to end if this is on
     pause_mode = Bool(False).tag(pref=True)
+
+    # : Create the entry which contains all the data return by the OPX in a recarray
+    database_entries = set_default({'Results': {}})
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -143,18 +147,35 @@ class ConfigureExecuteTask(InstrumentTask):
                     logger.warning(e)
 
             for (name, handle) in results:
-                if name.endswith('_input1') or name.endswith('_input2'):
-                    name = name[:-7]
-                data = handle.fetch_all()
-                # When using the old school syntax for saving, the value is returned alongside the timestamp
-                # when this is the case, we discard the timestamp
-                try:
-                    data = data['value']
-                except (TypeError, IndexError):
-                    pass
-                self.write_in_database(f"variable_{name}", data)
-                if handle.has_dataloss():
-                    logger.warning(f"{name} might have data loss")
+                # Create the recarray to save the data
+                dt_array = []
+                for (name, handle) in results:
+                    if name.endswith('_input1') or name.endswith('_input2'):
+                        name = name[:-7]
+                    data = handle.fetch_all()
+                    # When using the old school syntax for saving, the value is returned alongside the timestamp
+                    # when this is the case, we discard the timestamp
+                    try:
+                        data = data['value']
+                    except (TypeError, IndexError):
+                        pass
+                    dt_array += [(name, data.dtype, data.shape)]
+                    if handle.has_dataloss():
+                        logger.warning(f"{name} might have data loss")
+                results_recarray = zeros(1, dtype=dt_array)
+
+                # Save data in the recarray
+                for (name, handle) in results:
+                    if name.endswith('_input1') or name.endswith('_input2'):
+                        name = name[:-7]
+                    data = handle.fetch_all()
+                    try:
+                        data = data['value']
+                    except (TypeError, IndexError):
+                        pass
+                    self.write_in_database(f"variable_{name}", data)
+                    results_recarray[name] = data
+                self.write_in_database('Results', results_recarray)
 
     def refresh_config(self):
         self._post_setattr_path_to_config_file(self.path_to_config_file,
--- exopy_qm/tasks/tasks/MeasureWithPauseTask.py
+++ exopy_qm/tasks/tasks/MeasureWithPauseTask.py
@@ -37,7 +37,7 @@ class MeasureWithPauseTask(InstrumentTask):
             results = self.driver.get_results()
             one_is_none = False
             for (name, handle) in results:
-                if handle.fetch_all() is None:
+                if handle.fetch_all() is None: # check is one entry of the data is None
                     time.sleep(0.01)
                     one_is_none = True
             if not one_is_none: # wait for all entries to be not None before continuing
@@ -50,22 +50,26 @@ class MeasureWithPauseTask(InstrumentTask):
         for (name, handle) in results:
             if name.endswith('_input1') or name.endswith('_input2'):
                 name = name[:-7]
-            values = handle.fetch_all()
+            data = handle.fetch_all()
+            # When using the old school syntax for saving, the value is returned alongside the timestamp
+            # when this is the case, we discard the timestamp
             try:
-                values = values['value']
+                data = data['value']
             except (TypeError, IndexError):
                 pass
-            dt_array += [(name, values.dtype, values.shape)]
+            dt_array += [(name, data.dtype, data.shape)]
+            if handle.has_dataloss():
+                logger.warning(f"{name} might have data loss")
         results_recarray = np.zeros(1, dtype=dt_array)
 
         # Save data in the recarray
         for (name, handle) in results:
             if name.endswith('_input1') or name.endswith('_input2'):
                 name = name[:-7]
-            values = handle.fetch_all()
+            data = handle.fetch_all()
             try:
-                values = values['value']
+                data = data['value']
             except (TypeError, IndexError):
                 pass
-            results_recarray[name] = values
+            results_recarray[name] = data
         self.write_in_database('Results', results_recarray)
